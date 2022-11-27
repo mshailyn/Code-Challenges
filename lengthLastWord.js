12afab43ed3cf89ae7b71ead2bea262b67b06473
Shailyn Moore <m.shailyn@yahoo.com>
2022-11-26 17:07:03 -0800

    new challenge

---
+++ lengthLastWord.js
@@ -0,0 +1,10 @@
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal substring consisting of non-space characters only.
+
+
+
+var lengthOfLastWord = function(s) {
+   s = s.trim().split(' ')
+   return s[s.length-1].length
+};
